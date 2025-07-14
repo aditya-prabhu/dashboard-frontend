d8d4628094d27fcb3c65daf7c31494a98f7d5f29
Prabhu, Aditya <aditya.prabhu+prov@providence.org>
2025-07-14 11:38:33 +0530

    upadted BE url

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,3 +1,3 @@
-const API_BASE = "https://func-dashboardbe-cveffbf7d9fpbcex.westus3-01.azurewebsites.net";
+const API_BASE = "https://fa-dashbordbe-b5a7f8akbzanhjcp.westus3-01.azurewebsites.net";
 
 export { API_BASE };
